4b2b6a9c431c4e4342f2052d97b6cd753a4c65a0
Christoph Klisch <klisch.christoph@gmail.com>
2015-12-08 15:59:46 +0100

    stated work on matrix for different particles (doesn't compile)

--- main.cpp
+++ main.cpp
@@ -6,12 +6,37 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include "bmp.h"
 
 #define SIZE 360
 #define SPEED 5
 #define TRANS 2000
 #define REC 2000
 #define ITER 100
+
+uint64_t** init_matrix()
+{
+	uint64_t** matrix = (uint64_t*) malloc(2*SIZE*8);
+	for (int i = 0; i < 2*SIZE; i++) {
+		matrix[i] = (uint64_t) malloc(2*SIZE*8);
+		for (int j = 0; j < 2*SIZE; j++) {
+			matrix[i][j] = WHITE;
+		}
+	}
+	return matrix;
+}
+
+void destroy_matrix()
+{
+	for (int i = 0; i < 2*SIZE; i++) {
+		free(matrix[i]);
+	}
+}
+
+void write_matrix(particle* p, uint64_t** matrix)
+{
+
+}
 
 
 int main () {
@@ -42,6 +67,7 @@ int main () {
 		t[i].rand_vel(2*i);
 		tm.push_back(t[i]);
 	}
+
 	for (int i = 0; i < ITER; i++) {
 		for (int j = 0; j < TRANS; j++) {
 			t[j].move();
